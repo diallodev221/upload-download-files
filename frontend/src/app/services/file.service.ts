4324a4f00c171e7ce0dedeca46f9e7cca7f5492c
diallodeveloper <diallocodeur@gmail.com>
2023-05-19 18:39:26 +0000

    add upload and dowmload funtionalities in services

--- frontend/src/app/services/file.service.ts
+++ frontend/src/app/services/file.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  private serverURI = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  // define a function to upload files
+  upload(formData: FormData): Observable<HttpEvent<String[]>> {
+    return this.http.post<String[]>(`${this.serverURI}/file/upload`, formData, {
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  // define a function to download files
+  download(filename: String): Observable<HttpEvent<Blob>> {
+    return this.http.get(`${this.serverURI}/file/download/${filename}`, {
+      reportProgress: true,
+      observe: 'events',
+      responseType: 'blob'
+    });
+  }
+
+  
 }

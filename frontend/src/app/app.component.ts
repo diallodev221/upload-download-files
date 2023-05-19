d53db5c98eabe04e54dba67b0099ad354e0ae460
diallodeveloper <diallocodeur@gmail.com>
2023-05-19 19:23:13 +0000

    add reportProgress and updateStatus functionalities

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FileService } from './services/file.service';
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
+import { FileStatus } from './model/filestatus';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  filenames: String[] = [];
+  fileStatus!: FileStatus;
+
+  constructor(private fileService: FileService) { }
+  
+  // upload files
+  onUploadFiles(files: File[]): void {
+    const formData = new FormData();
+    for (const file of files) {
+      formData.append('files', file, file.name);
+    }
+    this.fileService.upload(formData).subscribe(event => {
+      console.log(event);
+      this.reportProgress(event);
+    },
+    (error: HttpErrorResponse) => console.log(error))
+  }
+  
+  // download files
+  onDownloadFiles(filename: String): void {
+    this.fileService.download(filename).subscribe(event => {
+      console.log(event);
+      this.reportProgress(event);
+    },
+    (error: HttpErrorResponse) => console.log(error))
+  }
+
+  private reportProgress(event: HttpEvent<String[] | Blob>): void {
+    switch (event.type) {
+      case HttpEventType.UploadProgress:
+        this.updateStatus(event.loaded, event.total!, 'Uploading');
+        break
+        case HttpEventType.DownloadProgress:
+          this.updateStatus(event.loaded, event.total!, 'Downloading');
+        break
+      case HttpEventType.Response:
+        if (event.body instanceof Array) {
+          for (const filename of event.body) {
+            this.filenames.unshift(filename)
+          }
+        } else {
+          // download logic
+          // add filesaver library
+          
+          // saveAs(new File(event.body), event.headers.get('file-name'), type: `${event.headers.get('content-Type')};charSet=utf-8`)
+
+        }
+        break;
+      default:
+        console.log(event);
+        
+        
+    }
+  }
+
+  private updateStatus(loaded: number, total: number, requestType: string) {
+    this.fileStatus.status = 'progress';
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = Math.round(100 * loaded / total);
+  }
+  
 }
---
+++ frontend/src/app/model/filestatus.ts
@@ -0,0 +1,5 @@
+export interface FileStatus {
+  status: string;
+  requestType: string;
+  percent: number;
+}
